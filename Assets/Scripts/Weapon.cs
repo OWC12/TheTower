c424aa71a07afc4e40b504d5493a833fd8c0ddd5
OWC12 <OWC12@pitt.edu>
2026-08-22 14:33:06 -0400

    debugging

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Weapons/Weapon")]
 public class Weapon : ScriptableObject
 {
-    public String weaponName {get; set;}
+    public string weaponName {get; set;}
     public Sprite weaponSprite {get; set;}
     public Dictionary<string, int> BaseDamage {get; set;} = new Dictionary<string, int>();
     public Dictionary<string, string> Attributes{get; set;} = new Dictionary<string, string>();
